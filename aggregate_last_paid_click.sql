e2b2a8b837225488315aa39f76214add90000d2d
LittleFritz83 <53961926+LittleFritz83@users.noreply.github.com>
2025-01-02 17:03:51 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,34 +1,90 @@
-select z2.visit_date, sum(z2.visitors_count) as visitors_count, z2.source as utm_source, z2.medium as utm_medium, z2.campaign as utm_campaign, coalesce(sum(z3.daily_spent), 0) as total_cost, 
-       sum(z2.leads_count) as leads_count, sum(z2.purchases_count) as purchases_count, sum(z2.revenue) as revenue
-from
-(select cast(visit_date as date) as visit_date, source, medium, campaign, content, count(*) as visitors_count, sum(lead_cnt) as leads_count, sum(purchase_cnt) as purchases_count, coalesce(sum(amount), 0) as revenue
-from
-(select x1.visitor_id, x1.visit_date, x1.source, x1.medium, x1.campaign, x1.content, x2.amount, x2.closing_reason, x2.status_id,
-       case when x2.amount is null then 0 else 1 end lead_cnt, 
-       case when x2.closing_reason = 'Успешно реализовано' or x2.status_id = 142 then 1 else 0 end purchase_cnt
-from
-(select distinct visitor_id, visit_date, source, medium, campaign, content 
-from sessions) as x1
-  left join (select visitor_id, visit_date, source, medium, campaign, content, amount, closing_reason, status_id 
-               from
-                 (select visitor_id, visit_date, source, medium, campaign, content, amount, closing_reason, status_id, paid_flag, 
-                         row_number( ) over(partition by visitor_id order by paid_flag, visit_date desc) as num 
-                    from
-                      (select leads.visitor_id, sessions.visit_date, sessions.source, sessions.medium, sessions.campaign, sessions.content, leads.amount, leads.closing_reason, leads.status_id, 
-                              case when sessions.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social') then 0 else 1 end paid_flag
-                         from leads inner join sessions on leads.visitor_id = sessions.visitor_id and leads.created_at >= sessions.visit_date) y1) y2
-               where num = 1) as x2   
-  on x1.visitor_id = x2.visitor_id and x1.visit_date = x2.visit_date and x1.source  = x2.source and 
-     x1.medium     = x2.medium     and x1.campaign   = x2.campaign   and x1.content = x2.content) z1
-group by cast(visit_date as date), source, medium, campaign, content) z2
-  left join (select cast(campaign_date as date) as camp_date, utm_source, utm_medium, utm_campaign, utm_content, sum(daily_spent) as daily_spent
-               from
-                 (select campaign_date, utm_source, utm_medium, utm_campaign, utm_content, daily_spent 
-                    from vk_ads
-                  union all 
-                  select campaign_date, utm_source, utm_medium, utm_campaign, utm_content, daily_spent
-                    from ya_ads) x1
-               group by cast(campaign_date as date), utm_source, utm_medium, utm_campaign, utm_content) z3
-  on z2.visit_date = z3.camp_date and z2.source = z3.utm_source and z2.medium = z3.utm_medium and z2.campaign = z3.utm_campaign and z2.content = z3.utm_content 
-  group by z2.visit_date, z2.source, z2.medium, z2.campaign
-  order by sum(z2.revenue) desc nulls last, z2.visit_date, sum(z2.visitors_count) desc, z2.source, z2.medium, z2.campaign;
+with purchases_tab1 as (
+select leads.visitor_id, sessions.visit_date, sessions.source, sessions.medium, sessions.campaign, sessions.content, leads.amount
+  from leads inner join sessions on leads.visitor_id = sessions.visitor_id and (leads.closing_reason = 'Успешно реализовано' or leads.status_id = 142) 
+  where sessions.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+purchases_tab2 as (
+select visitor_id, visit_date, source, medium, campaign, content, amount, 
+       row_number( ) over(partition by visitor_id order by visit_date desc) as num 
+  from purchases_tab1
+),
+purchases_tab3 as (
+select cast(visit_date as date) as visit_date, source, medium, campaign, count(*) as purchases_count, sum(amount) as revenue 
+  from purchases_tab2
+  where num = 1
+    and medium != 'cpa'
+group by cast(visit_date as date), source, medium, campaign    
+),
+leads_tab1 as (
+select distinct leads.visitor_id, sessions.visit_date, sessions.source, sessions.medium, sessions.campaign, sessions.content 
+  from leads inner join sessions on leads.visitor_id = sessions.visitor_id and leads.created_at >= sessions.visit_date 
+  where sessions.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+leads_tab2 as (  
+select visitor_id, visit_date, source, medium, campaign, content, 
+       row_number( ) over(partition by visitor_id order by visit_date desc) as num
+from leads_tab1
+),
+leads_tab3 as (
+select cast(visit_date as date) as visit_date, source, medium, campaign, count(visitor_id) as leads_count
+from leads_tab2
+where num = 1
+group by cast(visit_date as date), source, medium, campaign
+),
+cost_tab1 as (
+select campaign_date, utm_source, utm_medium, utm_campaign, utm_content, daily_spent 
+from vk_ads
+union all 
+select campaign_date, utm_source, utm_medium, utm_campaign, utm_content, daily_spent
+from ya_ads
+),
+cost_tab2 as (
+select cast(campaign_date as date) as campaign_date, utm_source, utm_medium, utm_campaign, sum(daily_spent) as total_cost
+from cost_tab1
+group by cast(campaign_date as date), utm_source, utm_medium, utm_campaign
+),
+visitors_tab1 as (
+select leads.visitor_id, sessions.visit_date, sessions.source, sessions.medium, sessions.campaign, sessions.content 
+  from leads inner join sessions on leads.visitor_id = sessions.visitor_id -- and leads.created_at >= sessions.visit_date 
+  where sessions.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+visitors_tab2 as (
+select visitor_id, visit_date, source, medium, campaign, content, 
+       row_number( ) over(partition by visitor_id order by visit_date desc) as num
+from visitors_tab1
+),
+visitors_tab3 as (
+select visitor_id, cast(visit_date as date) as visit_date, source, medium, campaign, content 
+from visitors_tab2
+where num = 1
+union
+select visitor_id, cast(visit_date as date) as visit_date, source, medium, campaign, content
+from sessions 
+where not exists(select visitor_id from leads where leads.visitor_id = sessions.visitor_id)
+),
+visitors_tab4 as
+(select visit_date, source, medium, campaign, count(distinct visitor_id) as visitors_count
+from visitors_tab3
+group by visit_date, source, medium, campaign
+),
+tab1 as (
+select distinct cast(visit_date as date) as visit_date, source, medium, campaign
+from sessions
+),
+tab2 as (
+select tab1.visit_date, 
+       coalesce(visitors_tab4.visitors_count, 0) as visitors_count,
+       tab1.source as utm_source, tab1.medium as utm_medium, tab1.campaign as utm_campaign,
+       coalesce(cast(cost_tab2.total_cost as text), '') as total_cost,
+       coalesce(leads_tab3.leads_count, 0) as leads_count, 
+       coalesce(purchases_tab3.purchases_count, 0) as purchases_count, coalesce(purchases_tab3.revenue, 0) as revenue
+from tab1
+  left join purchases_tab3 on tab1.visit_date = purchases_tab3.visit_date and tab1.source = purchases_tab3.source and tab1.medium = purchases_tab3.medium and tab1.campaign = purchases_tab3.campaign
+  left join leads_tab3     on tab1.visit_date = leads_tab3.visit_date     and tab1.source = leads_tab3.source     and tab1.medium = leads_tab3.medium     and tab1.campaign = leads_tab3.campaign
+  left join cost_tab2      on tab1.visit_date = cost_tab2.campaign_date   and tab1.source = cost_tab2.utm_source  and tab1.medium = cost_tab2.utm_medium  and tab1.campaign = cost_tab2.utm_campaign
+  left join visitors_tab4  on tab1.visit_date = visitors_tab4.visit_date  and tab1.source = visitors_tab4.source  and tab1.medium = visitors_tab4.medium  and tab1.campaign = visitors_tab4.campaign
+)
+select *
+from tab2
+order by revenue desc, visit_date, visitors_count desc, utm_source, utm_medium, utm_campaign;
+
